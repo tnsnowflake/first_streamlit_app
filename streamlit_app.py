bc759c777e4494d047a590ce2f1cf50adff883b8
Tom Nugent <122041835+tnsnowflake@users.noreply.github.com>
2023-01-10 14:37:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Shows the normalized data fame
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
